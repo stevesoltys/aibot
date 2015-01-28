edff70cdbba576a6867b3ae6db83457788514539
Steve <stevesoltys@live.com>
2015-01-28 18:45:24 -0500

    The response plugin now reacts to any message towards the bot. However, this requires some hardcoding and I don't like it. Will need to revisit this.

--- lib/aibot/protocol/irc/plugins/respond.rb
+++ lib/aibot/protocol/irc/plugins/respond.rb
@@ -4,10 +4,14 @@ module Cinch
     # Cinch plugin which utilizes the response algorithm to respond to users.
     class Response
       include Cinch::Plugin
+      PLUGIN_NAMES = ['urban', 'rr', 'word'] # we want to avoid matching other plugins
 
-      match /(reply|talk about|talk|speak|respond|tell me about|tell me)(.*)/
+      match /(reply|talk about|talk|speak|respond|tell me about|tell me)*(.*)/
 
       def execute(msg, command, topic)
+        PLUGIN_NAMES.each do |name| #TODO: figure out a better way!!
+          return if topic.downcase.start_with? name
+        end
         msg.safe_reply msg.bot.respond(topic)
       end
     end
