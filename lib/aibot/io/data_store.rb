a56c3a740d569088d95e4df19c245880df939835
Steve <stevesoltys@live.com>
2015-01-30 12:28:41 -0500

    Remove serialized_data_store.rb and change the way data stores work. They now have transactions performed on them, rather than having to query the data store multiple times. (Which was very slow)

--- lib/aibot/io/data_store.rb
+++ lib/aibot/io/data_store.rb
@@ -1,38 +1,20 @@
 module AIBot
   class DataStore
     ##
-    # Gets the list of keys for this data store.
-    def keys
+    # Performs a transaction on this data store.
+    def transaction(&block)
       raise 'SubclassResponsibility'
     end
 
     ##
-    # Checks if this data store has a given key.
-    def has?(key)
-      raise 'SubclassResponsibility'
-    end
-
-    ##
-    # Gets data for a given key from this store.
-    def get(key)
-      raise 'SubclassResponsibility'
-    end
-
-    ##
-    # Puts data in this store, given the key and value.
-    def put(key, value)
-      raise 'SubclassResponsibility'
-    end
-
-    ##
-    # Loads our data. This is the responsibility of the
+    # Loads our data store. This is the responsibility of the
     # subclass.
     def load(params=nil)
       raise 'SubclassResponsibility'
     end
 
     ##
-    # Saves our data. This is the responsibility of the
+    # Saves our data store. This is the responsibility of the
     # subclass.
     def save(params=nil)
       raise 'SubclassResponsibility'
