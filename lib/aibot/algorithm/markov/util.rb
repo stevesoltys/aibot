cd60d63a7e93994a132e17b4f8ccbe5acc456f89
Steve <stevesoltys@live.com>
2015-05-12 20:43:10 -0400

    Change markov algorithm slightly, it now selects more specific responses instead of searching for every word in an input sentence.

--- lib/aibot/algorithm/markov/util.rb
+++ lib/aibot/algorithm/markov/util.rb
@@ -23,15 +23,15 @@ module AIBot::Algorithm::Markov
     # Gets the trigram hash for a sentence.
     def get_trigram_hash(sentence)
       sentence = sentence.downcase.strip.remove_punctuation.split
-      pair_hash = {}
+      trigram_hash = {}
       if sentence.size >= 3
-        current_pair = sentence[0..1]
+        current_trigram = sentence[0..1]
         sentence[2..sentence.length].each do |word|
-          pair_hash[current_pair.clone] = word
-          current_pair.shift
-          current_pair << word
+          trigram_hash[current_trigram.clone] = word
+          current_trigram.shift
+          current_trigram << word
         end
-        pair_hash
+        trigram_hash
       elsif sentence.size == 2
         {[sentence[0], sentence[1]] => []}
       else
@@ -40,18 +40,31 @@ module AIBot::Algorithm::Markov
     end
 
     ##
-    # Gets an array of pair that is bias for words that are in the given sentence. If it cannot find any pairs for the
-    # given sentence, it will choose at random.
+    # Gets an array of trigrams that is bias for words that are in the given sentence. If it cannot find any trigrams
+    # for the given sentence, it will choose at random.
     def get_start_trigrams(data_store, sentence)
       trigrams = []
 
       # downcase, strip whitespace, and remove any punctuation from the input sentence
       sentence = sentence.downcase.strip.remove_punctuation
 
-      # look for trigrams which contain words in the sentence and add them
+      # generate a trigram hash for this sentence
+      sentence_trigram_hash = get_trigram_hash(sentence)
+
+      # look for trigrams which are entirely contained in the sentence and add them
+      sentence_trigram_hash.each do |pair, word|
+        trigrams.concat(data_store.execute("SELECT * FROM markov_trigrams WHERE first='#{pair[0]}' AND second='#{pair[1]}' AND third='#{word}'"))
+      end
+
+      # look for trigrams which contain pairs in the sentence and add them, if the current list is empty
+      sentence_trigram_hash.each do |pair, word|
+        trigrams.concat(data_store.execute("SELECT * FROM markov_trigrams WHERE first='#{pair[0]}' AND second='#{pair[1]}'"))
+      end if trigrams.empty?
+
+      # look for trigrams which contain any words in the sentence and add them, if the current list is empty
       sentence.split.each do |word|
         trigrams.concat(data_store.execute("SELECT * FROM markov_trigrams WHERE first='#{word}' OR second='#{word}'"))
-      end
+      end if trigrams.empty?
 
       # if we didn't find anything, we add a random trigram
       trigrams.concat(data_store.execute('SELECT * FROM markov_trigrams ORDER BY RANDOM() LIMIT 1')) if trigrams.empty?
