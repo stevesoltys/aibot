5460d76b0423cf8923500fd69c14501aa3c7338e
Steve <stevesoltys@live.com>
2015-09-24 14:30:46 -0400

    Revert last change, but increase maximum response size.

--- lib/aibot/algorithm/markov/markov.rb
+++ lib/aibot/algorithm/markov/markov.rb
@@ -40,7 +40,7 @@ module AIBot::Algorithm::Markov
       # add a random amount of connectable words before our input quad. the maximum is five.
       current_quad = input_quad
 
-      while true
+      18.times do
         before_quad = connectable_quad_for(data_store, current_quad, :before)
         break if before_quad.nil?
 
@@ -52,7 +52,7 @@ module AIBot::Algorithm::Markov
       # add a random amount of connectable words after our input quad. the maximum is five.
       current_quad = input_quad
 
-      while true
+      18.times do
         after_quad = connectable_quad_for(data_store, current_quad, :after)
         break if after_quad.nil?
 
