6ee70a1cd569f75b7d3bfb2935859b24ffed3fea
Steve Soltys <stevesoltys@live.com>
2015-11-18 10:27:54 -0500

    Optimize Markov algorithm database schema.

--- lib/aibot/algorithm/markov/markov.rb
+++ lib/aibot/algorithm/markov/markov.rb
@@ -7,9 +7,10 @@ module AIBot::Algorithm::Markov
     include MarkovUtils
 
     ##
-    # Intialize the database.
+    # Initialize the database.
     def init(data_store)
       data_store.execute('CREATE TABLE IF NOT EXISTS markov_quads(first TEXT NOT NULL, second TEXT NOT NULL, third TEXT NOT NULL, fourth TEXT NOT NULL)')
+      data_store.execute('CREATE TABLE IF NOT EXISTS markov_links(first TEXT NOT NULL, second TEXT NOT NULL, third TEXT NOT NULL, before TEXT NOT NULL, after TEXT NOT NULL, PRIMARY KEY (first, second, third))')
     end
 
     ##
@@ -22,44 +23,81 @@ module AIBot::Algorithm::Markov
     # Learns from the given input, if it is deemed worth learning.
     def learn(data_store, input)
       data_store.transaction do |store|
+
         quad_hash_for(input.downcase).each do |pair, word|
           store.execute('INSERT OR IGNORE INTO markov_quads VALUES(?, ?, ?, ?)', [pair[0], pair[1], pair[2], word])
+
+          store.execute('INSERT OR IGNORE INTO markov_links VALUES(?, ?, ?, ?, ?)', [pair[0], pair[1], pair[2], '', ''])
+          store.execute('INSERT OR IGNORE INTO markov_links VALUES(?, ?, ?, ?, ?)', [pair[1], pair[2], pair[3], '', ''])
+
+          query = 'SELECT * FROM markov_links WHERE first=? AND second=? AND third=? LIMIT 1'
+
+          before_link = store.execute(query, [pair[0], pair[1], pair[2]]).first
+
+          after = before_link[4].split(' ')
+
+          unless after.include?(word)
+            after << word
+            after = after.join(' ')
+
+            update_query = 'UPDATE markov_links SET after=? WHERE first=? AND second=? AND third=?'
+            store.execute(update_query, [after, pair[0], pair[1], pair[2]])
+          end
+
+          after_link = store.execute(query, [pair[1], pair[2], word]).first
+
+          before = after_link[3].split(' ')
+
+          unless before.include?(pair[0])
+            before << pair[0]
+            before = before.join(' ')
+
+            update_query = 'UPDATE markov_links SET before=? WHERE first=? AND second=? AND third=?'
+            store.execute(update_query, [before, pair[1], pair[2], word])
+          end
         end
+
       end if should_learn(input)
     end
 
     ##
     # Responds to the given input.
     def respond(data_store, input, context)
+
       # a quad which is bias towards words in our input.
       input_quad = bias_quad_for(data_store, input)
 
       # start our response with the input quad.
       response = "#{input_quad[0]} #{input_quad[1]} #{input_quad[2]} #{input_quad[3]}"
 
-      # add a random amount of connectable words before our input quad. the maximum is five.
+      # add a random amount of connectible words before our input quad.
       current_quad = input_quad
 
-      18.times do
-        before_quad = connectable_quad_for(data_store, current_quad, :before)
-        break if before_quad.nil?
+      data_store.transaction do |store|
 
-        current_quad = before_quad
+        18.times do
+          before_quad = connectable_quad_for(store, current_quad, :before)
+          break if before_quad.nil?
 
-        response = "#{current_quad[0]} #{response}"
+          current_quad = before_quad
+
+          response = "#{current_quad[0]} #{response}"
+        end
+
+        # add a random amount of connectible words after our input quad.
+        current_quad = input_quad
+
+        18.times do
+          after_quad = connectable_quad_for(store, current_quad, :after)
+          break if after_quad.nil?
+
+          current_quad = after_quad
+
+          response = "#{response} #{current_quad[3]}"
+        end
+
       end
 
-      # add a random amount of connectable words after our input quad. the maximum is five.
-      current_quad = input_quad
-
-      18.times do
-        after_quad = connectable_quad_for(data_store, current_quad, :after)
-        break if after_quad.nil?
-
-        current_quad = after_quad
-
-        response = "#{response} #{current_quad[3]}"
-      end
 
       return response.strip
     end
--- lib/aibot/algorithm/markov/util.rb
+++ lib/aibot/algorithm/markov/util.rb
@@ -38,25 +38,36 @@ module AIBot::Algorithm::Markov
       # get all quads for the input sentence
       quads = quad_hash_for(sentence)
 
+      # the result quad
+      result = nil
+
       # iterate through the quads, attempting to find a quad which includes three words from the input quad.
-      quads.keys.shuffle.each do |pair|
-        if pair.size > 2
-          query = 'SELECT * FROM markov_quads WHERE first=? AND second=? AND third=? ORDER BY RANDOM() LIMIT 1'
+      data_store.transaction do |store|
+        quads.keys.shuffle.each do |pair|
+          if pair.size > 2
+            query = 'SELECT * FROM markov_quads WHERE first=? AND second=? AND third=? ORDER BY RANDOM() LIMIT 1'
 
-          quad = data_store.execute(query, [pair[0], pair[1], pair[2]]).first
+            result = store.execute(query, [pair[0], pair[1], pair[2]]).first
 
-          return quad unless quad.nil?
+            break unless result.nil?
+          end
         end
       end
 
+      return result unless result.nil?
+
       # iterate through the quads, attempting to find a quad which includes two words from the input quad.
-      quads.keys.shuffle.each do |pair|
-        query = 'SELECT * FROM markov_quads WHERE first=? AND second=? ORDER BY RANDOM() LIMIT 1'
+      data_store.transaction do |store|
+        quads.keys.shuffle.each do |pair|
+          query = 'SELECT * FROM markov_quads WHERE first=? AND second=? ORDER BY RANDOM() LIMIT 1'
 
-        quad = data_store.execute(query, [pair[0], pair[1]]).first
+          result = store.execute(query, [pair[0], pair[1]]).first
 
-        return quad unless quad.nil?
+          break unless result.nil?
+        end
       end
+
+      return result unless result.nil?
 
       # if we couldn't find a quad match, we get a list of words in the sentence and look for a match
       words = sentence.split
@@ -65,13 +76,17 @@ module AIBot::Algorithm::Markov
       words.each { |word| words.delete(word) unless word.size >= 3 }
 
       # iterate through the words, attempting to find a quad which includes our given input word.
-      words.shuffle.each do |word|
-        query = 'SELECT * FROM markov_quads WHERE first=? OR second=? OR third=? OR fourth=? ORDER BY RANDOM() LIMIT 1'
+      data_store.transaction do |store|
+        words.shuffle.each do |word|
+          query = 'SELECT * FROM markov_quads WHERE first=? OR second=? OR third=? OR fourth=? ORDER BY RANDOM() LIMIT 1'
 
-        quad = data_store.execute(query, [word, word, word, word]).first
+          result = store.execute(query, [word, word, word, word]).first
 
-        return quad unless quad.nil?
+          break unless result.nil?
+        end
       end
+
+      return result unless result.nil?
 
       # if nothing was found, select a random quad.
       return data_store.execute('SELECT * FROM markov_quads ORDER BY RANDOM() LIMIT 1').first
@@ -82,11 +97,25 @@ module AIBot::Algorithm::Markov
     def connectable_quad_for(data_store, quad, type)
       case type
         when :before
-          query = 'SELECT * FROM markov_quads WHERE second=? AND third=? AND fourth=? ORDER BY RANDOM() LIMIT 1'
-          return data_store.execute(query, [quad[0], quad[1], quad[2]]).first
+          query = 'SELECT * FROM markov_links WHERE first=? AND second=? AND third=? ORDER BY RANDOM() LIMIT 1'
+
+          link = data_store.execute(query, [quad[0], quad[1], quad[2]]).first
+          return nil if link.nil?
+
+          before_list = link[3].split(' ')
+          return nil if before_list.empty?
+
+          return [before_list.sample, quad[0], quad[1], quad[2]]
         when :after
-          query = 'SELECT * FROM markov_quads WHERE first=? AND second=? AND third=? ORDER BY RANDOM() LIMIT 1'
-          return data_store.execute(query, [quad[1], quad[2], quad[3]]).first
+          query = 'SELECT * FROM markov_links WHERE first=? AND second=? AND third=? ORDER BY RANDOM() LIMIT 1'
+
+          link = data_store.execute(query, [quad[1], quad[2], quad[3]]).first
+          return nil if link.nil?
+
+          after_list = link[4].split(' ')
+          return nil if after_list.empty?
+
+          return [quad[1], quad[2], quad[3], after_list.sample]
         else
           raise 'Invalid quad connection type given!'
       end
